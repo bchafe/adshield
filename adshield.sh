daab6ee1ab194838278a4f9dcebc62b40ada3142
bchafe <39773821+bchafe@users.noreply.github.com>
2018-05-30 15:02:19 -0300

    Update adshield.sh
    Now a working implementation! Ready to test.

--- adshield.sh
+++ adshield.sh
@@ -3,16 +3,25 @@
 #
 # AdShield Script
 # Written by Benjamin Chafe
-echo "AdShield 1.0"
-
+echo "AdShield 1.1"
+# Now a working implementation!
+# Still requires testing to make sure everything works in abnormal
+# situations.
+#
+# TO DO: add an uninstall option
+# TO DO: Test in weird situation to make sure it doesnt break horribly.
+#
+#
+#
+#
+# Note: This script requires sudo and curl.
 
 
 
 CONFADD="https://raw.githubusercontent.com/bchafe/adshield/master/.adshieldrc"
 # CONFIG - This variable holds the location of the config
 # file. This file holds different settings for variables you can change.
-CONF="$SUDO_USER/.adshieldrc"
-source $CONF
+CONF="/home/$SUDO_USER/.adshieldrc"
 
 
 
@@ -27,6 +36,7 @@ then
   if [ $? == 0 ]
   then
     echo "Config successfully downloaded."
+    source $CONF
 
   else
     echo "Error: Downloading config file failed. Make sure 'curl' is"
@@ -38,11 +48,13 @@ fi
 
 if ! [ -e $DIR ]
 then
+  source $CONF
+
   echo "$DIR not found, Making directory..."
   mkdir $DIR
 
 
-  if [ $? == 0]
+  if [ $? == 0 ]
   then
 
     echo "Successful."
@@ -109,12 +121,28 @@ then
 
 
 
-
 elif [ "curl $ADDRESS | diff $PATCH -" == 1 ]
 then
 
+  source $CONF
+
+  touch $PATCH
+  if [ $? == 0 ]
+  then
+
+    echo "Successful."
+
+  else
+
+    echo "Failed."
+    exit 1
+
+  fi
+
 
   curl $ADDRESS > $PATCH
+
+
   if [ $? == 0 ]
   then
 
